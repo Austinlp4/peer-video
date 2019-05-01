38acc1d85c844d72a8e84d8879a6535aadaa577a
Austinlp4 <apendergrast44@gmail.com>
2019-05-01 16:40:22 -0500

    initial commit

---
+++ video/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WebRTCPeerConnection from './components/PeerConnection/WebRTCPeerConnection';
+
+
+function App() {
+  return (
+    <div className="App">
+      <WebRTCPeerConnection />
+    </div>
+  );
+}
+
+export default App;
