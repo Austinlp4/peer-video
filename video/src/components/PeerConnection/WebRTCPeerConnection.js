9034f44ebdc652270739f9e914e946b4d617df42
Austinlp4 <apendergrast44@gmail.com>
2019-05-01 16:54:03 -0500

    addition

--- video/src/components/PeerConnection/WebRTCPeerConnection.js
+++ video/src/components/PeerConnection/WebRTCPeerConnection.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React from "react";
+import "webrtc-adapter";
 
-class WebRTCPeerConnection extends React.Component{
+// copied from common.js https://github.com/webrtc/samples/blob/gh-pages/src/js/common.js
+function trace(arg) {
+    var now = (window.performance.now() / 1000).toFixed(3);
+    console.log(now + ": ", arg);
+}
+
+class WebRTCPeerConnection extends React.Component {
     state = {
         startDisabled: false,
         callDisabled: true,
@@ -18,21 +25,28 @@ class WebRTCPeerConnection extends React.Component{
         this.setState({
             startDisabled: true
         });
-        navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true
-        })
-        .then(this.gotStream)
-        .catch(err => alert("getUserMedia() error: " + err.name))
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: true,
+                video: true
+            })
+            .then(this.gotStream)
+            .catch(e => alert("getUserMedia() error:" + e.name));
     };
-
     gotStream = stream => {
         this.localVideoRef.current.srcObject = stream;
         this.setState({
             callDisabled: false,
             localStream: stream
-        })
-    }
+        });
+    };
+    gotRemoteStream = event => {
+        let remoteVideo = this.remoteVideoRef.current;
+
+        if (remoteVideo.srcObject !== event.streams[0]) {
+            remoteVideo.srcObject = event.streams[0];
+        }
+    };
 
     call = () => {
         this.setState({
@@ -41,11 +55,13 @@ class WebRTCPeerConnection extends React.Component{
         });
         let { localStream } = this.state;
 
+        // Ah, servers needs to be some sort of iceServer thing
+        // https://developer.mozilla.org/en-US/docs/Web/API/RTCIceServer
         let servers = null,
-            pc1 = new RTCPeerConnection(servers),
+            pc1 = new RTCPeerConnection(servers), // what is servers??
             pc2 = new RTCPeerConnection(servers);
 
-        pc1.onicecandidate = e => this.onicecandidate(pc1, e);
+        pc1.onicecandidate = e => this.onIceCandidate(pc1, e);
         pc1.oniceconnectionstatechange = e => this.onIceStateChange(pc1, e);
 
         pc2.onicecandidate = e => this.onIceCandidate(pc2, e);
@@ -55,26 +71,28 @@ class WebRTCPeerConnection extends React.Component{
         localStream
             .getTracks()
             .forEach(track => pc1.addTrack(track, localStream));
-        
+
         pc1
             .createOffer({
                 offerToReceiveAudio: 1,
                 offerToReceiveVideo: 1
             })
-            .then(this.onCreateOfferSuccess, error => 
+            .then(this.onCreateOfferSuccess, error =>
                 console.error(
-                    'Failed to create session description',
+                    "Failed to create session description",
                     error.toString()
                 )
             );
+
+        console.log("servers after call", servers);
 
         this.setState({
             servers,
             pc1,
             pc2,
             localStream
-        })
-    }
+        });
+    };
 
     onCreateOfferSuccess = desc => {
         let { pc1, pc2 } = this.state;
@@ -83,59 +101,59 @@ class WebRTCPeerConnection extends React.Component{
             .setLocalDescription(desc)
             .then(
                 () =>
-                    console.log('pc1 setLocalDescription complete createOffer'),
-                error => 
+                    console.log("pc1 setLocalDescription complete createOffer"),
+                error =>
                     console.error(
-                        'pc1 Failed to set session description in createOffer',
+                        "pc1 Failed to set session description in createOffer",
                         error.toString()
                     )
             );
-        
+
         pc2.setRemoteDescription(desc).then(
             () => {
-                console.log('pc2 setRemoteDescription complete createOffer');
+                console.log("pc2 setRemoteDescription complete createOffer");
                 pc2
                     .createAnswer()
                     .then(this.onCreateAnswerSuccess, error =>
                         console.error(
-                            'pc2 Failed to set session description in createAnswer',
+                            "pc2 Failed to set session description in createAnswer",
                             error.toString()
                         )
-                        )
+                    );
             },
-            error => 
+            error =>
                 console.error(
-                    'pc2 Failed to set session description in createOffer',
+                    "pc2 Failed to set session description in createOffer",
                     error.toString()
                 )
-        )
-    }
+        );
+    };
 
     onCreateAnswerSuccess = desc => {
         let { pc1, pc2 } = this.state;
-        
+
         pc1.setRemoteDescription(desc).then(
             () => {
-                console.log('pc1 setRemoteDescription complete createAnswer');
-                console.log('servers after createAnswer', this.state.servers);
+                console.log("pc1 setRemoteDescription complete createAnswer");
+                console.log("servers after createAnswer", this.state.servers);
             },
-            error => 
+            error =>
                 console.error(
-                    'pc1 Failed to set session description in onCreateAnswer',
+                    "pc1 Failed to set session description in onCreateAnswer",
                     error.toString()
                 )
-        )
+        );
 
         pc2
             .setLocalDescription(desc)
             .then(
-                () => 
-                console.log(
-                    'pc2 setLocalDescription complete createAnswer'
-                ),
-                error => 
+                () =>
+                    console.log(
+                        "pc2 setLocalDescription complete createAnswer"
+                    ),
+                error =>
                     console.error(
-                        'pc2 Failed to set session description in onCreateAnswer',
+                        "pc2 Failed to set session description in onCreateAnswer",
                         error.toString()
                     )
             );
@@ -149,19 +167,18 @@ class WebRTCPeerConnection extends React.Component{
         otherPc
             .addIceCandidate(event.candidate)
             .then(
-                () => console.log('addIceCandidate success'),
-                error => 
+                () => console.log("addIceCandidate success"),
+                error =>
                     console.error(
-                        'failed to add ICE Candidate',
+                        "failed to add ICE Candidate",
                         error.toString()
                     )
             );
     };
 
     onIceStateChange = (pc, event) => {
-        console.log('ICE state:', pc.iceConnectionState);
+        console.log("ICE state:", pc.iceConnectionState);
     };
-
 
     hangUp = () => {
         let { pc1, pc2 } = this.state;
@@ -174,37 +191,44 @@ class WebRTCPeerConnection extends React.Component{
             pc2: null,
             hangUpDisabled: true,
             callDisabled: false
-        })
-    }
+        });
+    };
 
-    render(){
+    render() {
         const { startDisabled, callDisabled, hangUpDisabled } = this.state;
-        return(
+
+        return (
             <div>
-                <video 
+                <video
                     ref={this.localVideoRef}
                     autoPlay
                     muted
-                    style={{ width: '240px', height: '180px' }}
-                />
-                <video 
+                    style={{
+                        width: "240px",
+                        height: "180px"
+                    }}
+                />{" "}
+                <video
                     ref={this.remoteVideoRef}
                     autoPlay
-                    style={{ width: '240px', height: '180px' }}
+                    style={{
+                        width: "240px",
+                        height: "180px"
+                    }}
                 />
                 <div>
                     <button onClick={this.start} disabled={startDisabled}>
-                        Start
-                    </button>
+                        Start{" "}
+                    </button>{" "}
                     <button onClick={this.call} disabled={callDisabled}>
-                        Call
-                    </button>
+                        Call{" "}
+                    </button>{" "}
                     <button onClick={this.hangUp} disabled={hangUpDisabled}>
-                        Hang Up
-                    </button>
-                </div>
+                        Hang Up{" "}
+                    </button>{" "}
+                </div>{" "}
             </div>
-        )
+        );
     }
 }
 
